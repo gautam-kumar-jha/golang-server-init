6c093aa11ab277fd85e3a4a10a561f2eb7c9f2fd
gautam-kumar-jha <gautamkumarjhagt@email.com>
2024-05-22 09:39:49 +0530

    test

--- goinittest/logic_test.go
+++ goinittest/logic_test.go
@@ -3,6 +3,7 @@ package goinittest
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"testing"
 )
@@ -13,11 +14,39 @@ func init() {
 	flag.StringVar(&path, "arg", "", "first argument")
 }
 func TestAdd(t *testing.T) {
-	te, _ := os.Getwd()
-	fmt.Println("Test:", path+te)
+	fmt.Println("Test:", path)
+
+	// Get a list of files in the directory
+	files, err := getFiles(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print the list of file names
+	for _, file := range files {
+		fmt.Println(file)
+	}
+
 	result := SayHello()
 	expected := "hello"
 	if result != expected {
 		t.Errorf("SayHello() = %s; want %s", result, expected)
 	}
+}
+
+func getFiles(dir string) ([]string, error) {
+	var files []string
+
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			files = append(files, entry.Name())
+		}
+	}
+
+	return files, nil
 }
